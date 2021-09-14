17d73b885ca124fb143f0658ff7632592024bf5e
Peter Kapteyn <kapteynpeter@gmail.com>
2021-09-14 18:32:31 -0400

    Added readme

---
+++ README.md
@@ -0,0 +1,27 @@
+
+# NOVIM
+
+## TUI library
+---
+https://github.com/chjj/blessed
+
+## Data flow excel doc
+---
+https://docs.google.com/spreadsheets/d/1QzQLfwKWLw71IFzB-DFeKvxMAVwAkV4MHKelF54-BoE/edit#gid=0
+
+## UML Diagram
+---
+Open the file `novim-uml.drawio` with this website
+
+https://app.diagrams.net/
+
+## Async keystate library
+---
+### node js library
+https://www.npmjs.com/package/asynckeystate
+
+### windows API doc
+https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getasynckeystate?redirectedfrom=MSDN
+
+### windows virtual keycode doc
+https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 
 
-while(true) {
-    const keys = query_keys();
-    console.log(keys.toString());
+function getKeysDepressed() {
+    const ret = [];
+    for(let i = 0; i < 256; i++) {
+        if(aks.getAsyncKeyState(i) == true) {
+            ret.push(i);
+        }
+    }
+    return ret
 }
 
-setInterval(function(){
-    console.log("Space Pressed", aks.getAsyncKeyState(0x20));
-}, 100);
+
+while(true) {
+    const keys = getKeysDepressed();
+    // parse keys
+    console.log(keys.toString());
+
+}
+
+// setInterval(function(){
+//     console.log("Space Pressed", aks.getAsyncKeyState(0x20));
+// }, 100);
+
+while (true){
+    get keys//
+}
